7f5b9eeb4eebe9b1d169d574375d6e76a43c836d
Rogi Solorzano <rogi.solorzano@gmail.com>
2023-03-14 14:59:00 -0500

    Add test decorator

--- examples.test.py
+++ examples.test.py
@@ -1,17 +1,21 @@
 import uasyncio as asyncio
-from microtest import expect, test, spy, async_spy
+from microtest import expect, test, run, spy, async_spy
 
+@test
 async def one_and_one_should_be_added():
   expect(1 + 1).to_be(2)
 
+@test
 async def arrays_should_be_deep_compared():
   expect([1, 's', ('tuple'), {"a": 1}]).to_be([1, 's', ('tuple'), {"a": 1}])
   expect([{"a": 1}]).it_not().to_be([{"a": 2}])
 
+@test
 async def it_should_not_have_called_the_spy():
   spy_instance = spy()
   expect(spy_instance).it_not().to_have_been_called()
 
+@test
 async def spies_should_be_called():
   spy_instance = spy()
   spy_instance()
@@ -20,6 +24,7 @@ async def spies_should_be_called():
   expect(spy_instance).it_not().to_have_been_called_times(1)
   expect(spy_instance).to_have_been_called_times(2)
 
+@test
 async def spies_should_be_called_with_args():
   spy_one = spy()
   spy_two = spy()
@@ -28,6 +33,11 @@ async def spies_should_be_called_with_args():
   expect(spy_one).to_have_been_called_with(1)
   expect(spy_two).to_have_been_called_with(1, 2)
   expect(spy_two).it_not().to_have_been_called_with(10)
+
+@test
+async def spy_should_return_value_that_was_set():
+  spy_one = spy().returns(10)
+  expect(spy_one()).to_be(10)
 
 class MockPublisher:
   def __init__(self):
@@ -40,6 +50,7 @@ class ExampleService:
   async def publish(self):
     await self.publisher.publish('message')
 
+@test
 async def example_service_should_publish_the_message():
   mock_publisher = MockPublisher()
   service = ExampleService(mock_publisher)
@@ -53,15 +64,8 @@ async def job():
   await asyncio.sleep(1)
   return 'it works!'
 
+@test
 async def test_should_await():
   expect(await job()).to_be('it works!')
 
-asyncio.run(test(
-  one_and_one_should_be_added,
-  arrays_should_be_deep_compared,
-  it_should_not_have_called_the_spy,
-  spies_should_be_called,
-  spies_should_be_called_with_args,
-  example_service_should_publish_the_message,
-  test_should_await
-))
+run()
